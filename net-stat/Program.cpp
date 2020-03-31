0d9ee1e9195b7550080d8f7dc072f9ed625013ca
Andrey Vakulich <andrey159159@gmail.com>
2020-03-31 11:35:59 +0300

    Stage 1

---
+++ net-stat/Program.cpp
@@ -0,0 +1,14 @@
+#include "Server.h"
+#include "Client.h"
+
+#include <iostream>
+
+int main(){
+	std::cout << "Hello" << std::endl;
+	Server *server = new Server();
+	Client *client = new Client();
+	delete server;
+	delete client;
+	std::cout << "Bye" << std::endl;
+	return 0;
+}
