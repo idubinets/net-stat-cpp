d27f6d7882b7b14339bb768b758baa65f14321d4
Andrey Vakulich <andrey159159@gmail.com>
2020-04-01 12:29:08 +0300

    Add CMakeLists.txt in engine-lib and net-stat

--- net-stat/Program.cpp
+++ net-stat/Program.cpp
@@ -4,11 +4,11 @@
 #include <iostream>
 
 int main(){
-	std::cout << "Hello" << std::endl;
+	std::cout << "Hello!" << std::endl;
 	Server *server = new Server();
 	Client *client = new Client();
 	delete server;
 	delete client;
-	std::cout << "Bye" << std::endl;
+	std::cout << "Bye!" << std::endl;
 	return 0;
 }
