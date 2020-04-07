9c762b8b8279cfb86813cec17383f0879c1640ec
Andrey Vakulich <andrey159159@gmail.com>
2020-04-07 13:27:59 +0300

    Add const string options, fix situation neither client nor server

--- net-stat/Program.cpp
+++ net-stat/Program.cpp
@@ -8,42 +8,47 @@ int main(int argc, char* argv[]){
 	try {
 		std::string ip;
 		int port;
+		const std::string helpOptionName = "help";
+		const std::string serverOptionName = "server";
+		const std::string clientOptionName = "client";
+		const std::string ipOptionName = "ip";
+		const std::string portOptionName = "port";
 
 		boost::program_options::options_description desc("options");
 		desc.add_options()
-			("help", "produce help message")
-			("server", "create server")
-			("client", "create client")
-			("ip", boost::program_options::value<std::string>(&ip)->default_value("127.0.0.1"), "set ip")
-			("port", boost::program_options::value<int>(&port)->default_value(30001), "set port")
+			(helpOptionName.c_str(), "produce help message")
+			(serverOptionName.c_str(), "create server")
+			(clientOptionName.c_str(), "create client")
+			(ipOptionName.c_str(), boost::program_options::value<std::string>(&ip)->default_value("127.0.0.1"), "set ip")
+			(portOptionName.c_str(), boost::program_options::value<int>(&port)->default_value(30001), "set port")
 			;
 		boost::program_options::variables_map vm;
 		boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
 		boost::program_options::notify(vm);
-
-
-		if (vm.count("help")) {
+		
+		if (vm.count(helpOptionName)) {
 			std::cout << "Usage: options_description [options]\n" << desc;
 			return 0;
 		}
-
-		if (vm.count("server"))
-		{
+		if (vm.count(serverOptionName)) {
 			Server server;
 			std::cout << "Server is running..." << std::endl;
 		}
-		else
-			if (vm.count("client"))
-			{
+		else {
+			if (vm.count(clientOptionName)) {
 				Client client;
 				std::cout << "Client is running..." << std::endl;
 			}
-
+			else {
+				std::cout << "Neither client nor server were selected" << std::endl;
+				return 1;
+			}
+		}
 		std::cout << "ip = " << ip << std::endl;
 		std::cout << "port = " << port << std::endl;
 	}
 	catch (const std::exception& e) {
-		std::cout << e.what() << "\n";
+		std::cout << e.what() << std::endl;
 		return 1;
 	}
 	return 0;
